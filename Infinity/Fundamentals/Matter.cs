d362d04d3cecbde8eb729eb480b12ad8956b388d
Jon Willis <jonathan.willis@newbalance.com>
2017-04-28 10:38:08 -0500

    BROKEN: Initial clean-up.

---
+++ Infinity/Fundamentals/Matter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Infinity.Interfaces;
+
+namespace Infinity.Fundamentals
+{
+    public abstract class Matter : IMatter
+    {
+        #region Members
+
+        public static Random Random => new Random();
+
+        private IDictionary<string, string> _properties;
+
+        private IList<IMatter> _children;
+
+        #endregion
+
+        #region Constructors
+
+        protected Matter()
+        {
+            // Initialize empty instance.
+        }
+
+        protected Matter(string name, string type, string subType)
+        {
+            ChildConfigs = new List<ChildConfig>();
+            Children = new List<Matter>();
+            Properties = new Dictionary<string, string>();
+
+            Name = name;
+            SubType = subType;
+            Type = type;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Initialize()
+        {
+            if (!IsInitialized)
+            {
+                var firstChild = ChildConfigs.FirstOrDefault(x => x.Name == SubType);
+                if (firstChild != null)
+                {
+                    foreach (var child in firstChild.Children)
+                    {
+                        MakeChildren(child);
+                    }
+                }
+
+                IsInitialized = true;
+            }
+        }
+
+        public void MakeChildren(IMatter child)
+        {
+            if (child.Min == 1 && child.Max == 1)
+            {
+                var childType = Choose(child.Classes);
+                if (childType != null)
+                {
+                    var newChild = Activator.CreateInstance(System.Type.GetType(childType)) as IMatter;
+                    newChild.Parent = this;
+
+                    Children.Add(newChild);
+                }
+            }
+            else
+            {
+                var total = Random.Next(child.Min, child.Max);
+
+                if (total != 0)
+                {
+                    for (var x = 0; x <= total; x++)
+                    {
+                        var childType = Choose(child.Classes);
+                        if (childType != null)
+                        {
+                            var newChild = Activator.CreateInstance(System.Type.GetType(Choose(child.Classes))) as IMatter;
+                            newChild.Parent = this;
+
+                            Children.Add(newChild);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected T Choose<T>(Dictionary<T, double> choices)
+        {
+            var newChoices = new List<T>();
+
+            foreach (var choice in choices)
+            {
+                // TODO: Figure out why this "magic number" of 10 exists?
+                for (var x = 0; x < choice.Value * 10; x++)
+                {
+                    newChoices.Add(choice.Key);
+                }
+            }
+
+            return newChoices[Random.Next(0, newChoices.Count - 1)];
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IList<IMatter> Children
+        {
+            get { return _children; }
+            set { _children = value; }
+        }
+
+        public bool IsInitialized { get; set; }
+
+        public string Name { get; set; }
+
+        public IMatter Parent { get; set; }
+
+        public IDictionary<string, string> Properties
+        {
+            get { return _properties; }
+            set { _properties = value; }
+        }
+
+        public string SubType { get; set; }
+
+        public string Type { get; set; }
+
+        #endregion
+    }
+}
--- Infinity/Program.cs
+++ Infinity/Program.cs
@@ -1,141 +1,227 @@
 ﻿using System;
-using System.Collections.Generic;
-using Infinity.Things;
 using System.Linq;
+using Infinity.Interfaces;
+using Infinity.Objects;
 
-namespace Infinity {
-    class Program {
-        static Thing position;
+namespace Infinity
+{
+    public static class Program
+    {
+        #region Members
 
-        static void Main(string[] args) {
+        private static IMatter _position;
+
+        #endregion
+
+        #region Methods
+
+        public static void Main(string[] args)
+        {
             Initialize();
+
+            while (ListenForUserInput())
+            {
+                // Weird...?
+            }
         }
 
-        static void Initialize() {
+        private static void Initialize()
+        {
             Console.ForegroundColor = ConsoleColor.DarkGray;
 
-            Thing.random = new Random();
+            _position = new Multiverse();
+            _position.Initialize();
 
-            List<Thing> things = new List<Thing>();
+            if (_position.Parent != null)
+            {
+                Display($"# & & & <y> Up <g> {_position.Parent.Type} <w> {_position.Parent.Name} </>");
+            }
 
-            things.Add(new Multiverse());
+            Display($"# & & & <g> {_position.Type} <w> {_position.Name} </> #");
 
-            position = things[0];
-            position.Interact();
+            var position = 0;
 
-            while (Loop()) { }
+            if (_position.Children.Any())
+            {
+                _position.Children.ToList().ForEach(child =>
+                {
+                    Display($"& & & <y> Down <c> {position} </> - <g> {child.Type} <w> {child.Name} </>");
+                    position++;
+                });
+            }
+            else
+            {
+                Display("& & & <r> No lower levels... </>");
+            }
         }
 
-        static bool Loop() {
+        private static bool ListenForUserInput()
+        {
             Display("# & Enter your command: ");
 
             Console.ForegroundColor = ConsoleColor.Magenta;
-            string[] command = Console.ReadLine().ToLower().Split(' ');
+            var input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return true;
+            }
+
+            var command = input.ToLower().Split(' ');
             Console.ForegroundColor = ConsoleColor.DarkGray;
 
-            if (command[0] == "exit" || command[0] == "")
-                return false;
-            else if (command[0] == "help") {
-                position.Print();
-            } else if (command[0] == "down") {
-                if (position.children.Count != 0) {
-                    int newPosition;
+            switch (command[0].ToLowerInvariant())
+            {
+                case "exit":
+                    return false;
 
-                    try {
-                        newPosition = int.Parse(command[1]);
-                    } catch (Exception) {
-                        if (position.children.Count() == 1)
-                            Display(string.Format("# & To move down, enter <m> down <c> 0 </> ..."));
-                        else
-                            Display(string.Format("# & To move down, enter <m> down </> and a valid number between <c> 0 </> and <c> {0} </> ...", (position.children.Count - 1)));
-                        return true;
-                    }
+                case "help":
+                    PrintMatter(_position);
+                    return true;
 
-                    if (newPosition < position.children.Count) {
-                        position = position.children[newPosition];
-                        Display(string.Format("# & Moved to <w> {0} </> !", position.name));
-                        position.Interact();
-                    } else {
-                        if (position.children.Count() == 1)
-                            Display(string.Format("# & To move down, enter <m> down <c> 0 </> ..."));
-                        else
-                            Display(string.Format("# & To move down, enter <m> down </> and a valid number between <c> 0 </> and <c> {0} </> ...", (position.children.Count - 1)));
-                        return true;
-                    }
-                } else
-                    Display("# & There are no places to which you can move down...");
-            } else if (command[0] == "up") {
-                if (position.parent != null) {
-                    position = position.parent;
-                    Display(string.Format("# & Moved to <w> {0} </> !", position.name));
-                    position.Interact();
-                } else
-                    Display("# & There is no place to which you can move up...");
-            } else {
-                Display(string.Format("# & <m> {0} </> is not a proper command...", command));
+                case "down":
+                    return MoveDown(command);
+
+                case "up":
+                    return MoveUp(command);
+
+                default:
+                    Display($"# & <m> {command} </> is not a proper command...");
+                    return true;
+            }
+        }
+
+        private static bool MoveUp(string[] command)
+        {
+            if (_position.Parent != null)
+            {
+                _position = _position.Parent;
+                Display($"# & Moved to <w> {_position.Name} </> !");
+                _position.Initialize();
+            }
+            else
+            {
+                Display("# & There is no place to which you can move up...");
             }
 
             return true;
+        }
+
+        private static bool MoveDown(string[] command)
+        {
+            if (_position.Children.Count != 0)
+            {
+                int newPosition;
+
+                try
+                {
+                    newPosition = int.Parse(command[1]);
+                }
+                catch (Exception)
+                {
+                    Display((
+                        _position.Children.Count == 1 ? "# & To move down, enter <m> down <c> 0 </> ..." : $"# & To move down, enter <m> down </> and a valid number between <c> 0 </> and <c> {(_position.Children.Count - 1)} </> ..."
+                    ));
+                    return true;
+                }
+
+                if (newPosition < _position.Children.Count)
+                {
+                    _position = _position.Children[newPosition];
+                    Display($"# & Moved to <w> {_position.Name} </> !");
+                    _position.Initialize();
+                }
+                else
+                {
+                    if (_position.Children.Count == 1)
+                        Display("# & To move down, enter <m> down <c> 0 </> ...");
+                    else
+                        Display($"# & To move down, enter <m> down </> and a valid number between <c> 0 </> and <c> {(_position.Children.Count - 1)} </> ...");
+                    return true;
+                }
+            }
+            else
+                Display("# & There are no places to which you can move down...");
+            return false;
         }
 
         /// <summary>
         /// Writes the specified string value to the standard output stream using custom formatting.
         /// </summary>
         /// <param name="text">The value to write.</param>
-        public static void Display(string text) {
-            string[] segments = text.Split(' ');
+        public static void Display(string text)
+        {
+            var segments = text.Split(' ');
 
-            for (int x = 0; x < segments.Count(); x++) {
-                int end = segments[x].Count() - 1;
+            for (var x = 0; x < segments.Length; x++)
+            {
+                var end = segments[x].Length - 1;
                 if (end == -1)
                     continue;
 
-                if (segments[x] == "#") {
-                    Console.Write("\n");
-                    continue;
-                } else if (segments[x] == "&") {
-                    Console.Write(" ");
-                    continue;
-                }
-
-                if (segments[x] == "<r>") {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    continue;
-                } else if (segments[x] == "<y>") {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    continue;
-                } else if (segments[x] == "<g>") {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    continue;
-                } else if (segments[x] == "<c>") {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    continue;
-                } else if (segments[x] == "<b>") {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    continue;
-                } else if (segments[x] == "<m>") {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    continue;
-                } else if (segments[x] == "<w>") {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    continue;
-                } else if (segments[x] == "</>") {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    continue;
+                switch (segments[x])
+                {
+                    case "#":
+                        Console.Write("\n");
+                        continue;
+                    case "&":
+                        Console.Write(" ");
+                        continue;
+                    case "<r>":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        continue;
+                    case "<y>":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        continue;
+                    case "<g>":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        continue;
+                    case "<c>":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        continue;
+                    case "<b>":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        continue;
+                    case "<m>":
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        continue;
+                    case "<w>":
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    case "</>":
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        continue;
+                    default:
+                        break;
                 }
 
                 Console.Write(segments[x]);
 
-                if ((segments[segments.Count() - 2] == "</>" && x == segments.Count() - 3) && segments[segments.Count() - 1] != "")
+                if ((segments[segments.Length - 2] == "</>" && x == segments.Length - 3) && segments[segments.Length - 1] != "")
+                {
                     continue;
+                }
 
                 Console.Write(" ");
             }
 
             Console.ForegroundColor = ConsoleColor.DarkGray;
 
-            if (segments[segments.Count() - 1] != "")
+            if (segments[segments.Length - 1] != "")
+            {
                 Console.Write("\n");
+            }
         }
+
+        private static void PrintMatter(IMatter matter)
+        {
+            Display($"# & & & <g> {matter.Type} <w> {matter.Name} </> #");
+
+            foreach (var property in matter.Properties)
+            {
+                Display($"& & & <m> {property.Key} </> - <c> {property.Value} </>");
+            }
+        }
+
+        #endregion
     }
 }
